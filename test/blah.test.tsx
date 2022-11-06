8c90ba9390bd6925e77c1d5d5b107cda4f55ae21
jocarrd <jorgecarrera98d@gmail.com>
2022-11-06 12:35:57 +0100

    fix: fix git config

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    // ReactDOM.render(<Thing />, div);
+
     ReactDOM.unmountComponentAtNode(div);
   });
 });
